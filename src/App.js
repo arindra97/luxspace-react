a1d992837832782e8c3f5b721caa6ddf5a35c9bc
Riko <muhammad.arindra>
2024-11-13 17:05:20 +0700

    setting up tailwind and remove unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./assets/css/app.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <p>Hello World!</p>
-            </header>
+            <h1 className="text-2xl text-blue-600"> Hello</h1>
         </div>
     );
 }

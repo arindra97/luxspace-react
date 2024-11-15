9f0e7d6ac24811c86d4a4244dd97e1b59899c27f
Riko <muhammad.arindra>
2024-11-15 11:01:05 +0700

    slicing UI html to react - Page Congratulation - Page 404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "pages/HomePage";
 import Details from "pages/Details";
 import Cart from "pages/Cart";
+import Congratulation from "pages/Congratulation";
+import NotFound from "pages/NotFound";
 
 function App() {
     return (
@@ -12,6 +14,8 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/categories/:idc" element={<Details />} />
                 <Route path="/cart" element={<Cart />} />
+                <Route path="/congratulation" element={<Congratulation />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
     );

9c0c135ff6a5e6b2093efe05afa2d8ab9525a268
Riko <muhammad.arindra>
2024-11-15 10:28:31 +0700

    slicing UI html to React - Page Cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import HomePage from "pages/HomePage";
 import Details from "pages/Details";
+import Cart from "pages/Cart";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/categories/:idc" element={<Details />} />
+                <Route path="/cart" element={<Cart />} />
             </Routes>
         </div>
     );
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -5,6 +5,7 @@ import Header from "parts/Header";
 import Sitemap from "parts/Sitemap";
 import Footer from "parts/Footer";
 import Breadcrumb from "components/Breadcrumb";
+
 import ProductDetails from "parts/Details/ProductDetails";
 import Suggestion from "parts/Details/Suggestion";
 
